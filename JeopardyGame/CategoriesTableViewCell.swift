a51b9b91f86312373b249d443acdb38ed2fed43a
Ryan Ofori <ryanofori1@gmail.com>
2019-12-04 16:22:47 -0500

    Categories
    Created an array of categories by number (user can replace array with actual categories)

---
+++ JeopardyGame/CategoriesTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriesTableViewCell.swift
+//  JeopardyGame
+//
+//  Created by Ryan Ofori on 12/4/19.
+//  Copyright © 2019 dirtbag. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var catLbl: UILabel!
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- JeopardyGame/ViewController.swift
+++ JeopardyGame/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  JeopardyGame
 //
-//  Created by dirtbag on 12/4/19.
+//  Created by Ryan Ofori on 12/4/19.
 //  Copyright © 2019 dirtbag. All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
