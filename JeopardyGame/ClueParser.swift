a6b51fa78ffb98f26851fe8126bfb4aac632fcb5
Me <Me>
2019-12-04 16:51:47 -0500

    feat: Category Parser is working, including a text case

---
+++ JeopardyGame/ClueParser.swift
@@ -0,0 +1,20 @@
+//
+//  ClueParser.swift
+//  JeopardyGame
+//
+//  Created by dirtbag on 12/4/19.
+//  Copyright © 2019 dirtbag. All rights reserved.
+//
+
+import Foundation
+
+final class ClueParser {
+    
+    func parse(data: Data) -> [ClueData] {
+        var clueData = [ClueData]()
+        
+        
+        return clueData
+    }
+    
+}
