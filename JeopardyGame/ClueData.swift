9d3c78d7aa2014d48f474729e3c01597b6585c51
Me <Me>
2019-12-05 16:15:43 -0500

    feat: Clue data being parsed.  Code cleanup on Category parser. Renamed category test file.

--- JeopardyGame/ClueData.swift
+++ JeopardyGame/ClueData.swift
@@ -9,5 +9,9 @@
 import Foundation
 
 struct ClueData: Codable {
-
-}
+    let id: Int
+    let answer: String
+    let question: String
+    let value: Int?  // Sometimes missing in the data, so Optional
+    let category_id: Int
+}  // TODO: do we need an default for value?
--- JeopardyGame/ClueParser.swift
+++ JeopardyGame/ClueParser.swift
@@ -10,11 +10,27 @@ import Foundation
 
 final class ClueParser {
     
-    func parse(data: Data) -> [ClueData] {
-        var clueData = [ClueData]()
+    func parse(data: Data, parsed: @escaping ([ClueData]) -> Void) {
+                    
+        // background the loading / parsing elements
+        DispatchQueue.global(qos: .userInitiated).async {
+
+            print (data.description)
+            
+            do {
+                // create decoder
+                let jsonDecoder = JSONDecoder()
+
+                // decode json into structs
+                let clueData = try jsonDecoder.decode([ClueData].self, from: data)
+                
+                parsed(clueData)
+                               
+            } catch {
+                print ("Error Parsing ClueData JSON: \(error.localizedDescription)")
+            }
+        }
         
-        
-        return clueData
     }
     
 }
